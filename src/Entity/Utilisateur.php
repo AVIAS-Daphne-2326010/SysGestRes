88d6564efbe482cde534ece27b9c13fcc68190b1
AVIAS-Daphne-2326010 <aviasdaphne@gmail.com>
2025-04-08 16:34:42 +0200

    configuration pour la base de données + entité utilisateur

---
+++ src/Entity/Utilisateur.php
@@ -0,0 +1,73 @@
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity]
+class Utilisateur
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $nom;
+
+    #[ORM\Column(type: 'string', length: 255, unique: true)]
+    private $email;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $mot_de_passe;
+
+    #[ORM\OneToMany(mappedBy: 'utilisateur', targerEntity: Reservation::class)]
+    private $reservations;
+
+    public function __construct()
+    {
+        $this->reservations = new ArrayCollection();
+    }
+
+    // Getter et Setter
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getMotDePasse(): ?string
+    {
+        return $this->mot_de_passe;
+    }
+
+    public function setMotDePasse(string $mot_de_passe): self
+    {
+        $this->mot_de_passe = password_hash($mot_de_passe, PASSWORD_BCRYPT);
+
+        return $this;
+    }
+}
