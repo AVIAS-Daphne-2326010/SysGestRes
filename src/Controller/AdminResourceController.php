58931358e2bae29012dcbe717466f47610fce9fc
AVIAS-Daphne-2326010 <aviasdaphne@gmail.com>
2025-05-13 12:06:29 +0200

    affichage du lien pour les créneaux de chaque ressource

--- src/Controller/AdminResourceController.php
+++ src/Controller/AdminResourceController.php
@@ -8,6 +8,7 @@ use App\Entity\BookingHistory;
 use App\Entity\UserAccount;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Controller\AdminTimeslotController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
